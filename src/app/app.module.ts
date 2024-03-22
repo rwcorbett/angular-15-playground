fe0de9f70dca0c21a978f82f4c8e157c8e33524f
Robb Corbett <rcorbett@wearescs.com>
2024-03-22 11:20:23 -0400

    basic routing installed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { Routes, RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { DummyDataModule } from './dummy/dummy-data/dummy-data.module';
+
 import { TabCLIComponent } from './tab-cli/tab-cli.component';
 import { TabBasicsComponent } from './tab-basics/tab-basics.component';
 import { CardLabelComponent } from './card-label/card-label.component';
+
 import { Logging } from './logger.service';
-import { DummyDataModule } from './dummy/dummy-data/dummy-data.module';
+
+const appRoutes: Routes = [
+  { path: 'CLI', component: TabCLIComponent },
+  { path: 'Basic', component: TabBasicsComponent },
+];
 
 @NgModule({
   declarations: [
@@ -17,9 +26,12 @@ import { DummyDataModule } from './dummy/dummy-data/dummy-data.module';
     CardLabelComponent
   ],
   imports: [
+    DummyDataModule,
     BrowserModule,
     AppRoutingModule,
-    DummyDataModule
+    RouterModule.forRoot(
+      appRoutes, { enableTracing: true }  // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
